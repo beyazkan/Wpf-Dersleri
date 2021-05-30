ac3fd7ddede5ce45e4475c1a06f28b13b0ec742e
Lanet <Lanet@DESKTOP-L162GD2>
2021-05-30 22:49:44 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.library;
+using WpfApp1.usercontrols;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ucLoad ucLoad;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ucLoad = new ucLoad();
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btn_kapat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SagUst_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void btn_kucult_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btn_KitapListesi_Click(object sender, RoutedEventArgs e)
+        {
+            ucLoad.uc_Load(content, new kitapListesi());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ucLoad.uc_Load(content, new kitapListesi());
+            DBConnection.connectionTest();
+            version.Content = DBConnection.connectionState;
+        }
+
+        private void btn_tamEkran_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+                mainWindow.Margin = new Thickness(0,0,0,0);
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                mainWindow.Margin = new Thickness(15, 15, 15, 15);
+            }
+            
+        }
+    }
+}
---
+++ WpfApp1/library/DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.library
+{
+    public class DBConnection
+    {
+        public static string dbPath = @"Data Source=" + Environment.CurrentDirectory + "\\Database\\kitap.db;Version=3;New=False;Compress=True;Read Only=False;";
+
+        public static string connectionState;
+
+        public static void connectionTest()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(dbPath))
+            {
+                if(conn.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        conn.Open();
+                        connectionState = "Veritabanına Bağlandı.";
+                    }
+                    catch (Exception)
+                    {
+                        connectionState = "Veritabanı Bağlantı Hatası...";
+                    }
+                }
+                else
+                {
+                    connectionState = "Veritabanına Bağlandı.";
+                }
+            } 
+        }
+    }
+}
